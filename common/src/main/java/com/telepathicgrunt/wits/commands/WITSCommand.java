06982fa6248baf7bbbb0fe251694ea979fc27a7a
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-10-09 07:39:16 -0400

    split out op command

--- common/src/main/java/com/telepathicgrunt/wits/commands/WITSCommand.java
+++ common/src/main/java/com/telepathicgrunt/wits/commands/WITSCommand.java
@@ -25,6 +25,7 @@ import java.util.List;
 public class WITSCommand {
     public static void createCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
         String commandString = "wits";
+        String opCommandString = "witsop";
         String dimensionArg = "dimension";
         String locationArg = "location";
 
@@ -43,6 +44,11 @@ public class WITSCommand {
                 listStructuresAtSpot(cs.getSource().getLevel(), coordinates, true, cs);
                 return 1;
             })
+        );
+
+        dispatcher.register(Commands.literal(commandString).redirect(source));
+
+        LiteralCommandNode<CommandSourceStack> source2 = dispatcher.register(Commands.literal(opCommandString)
             .requires((permission) -> permission.hasPermission(2))
             .then(Commands.argument(dimensionArg, DimensionArgument.dimension())
             .then(Commands.argument(locationArg, Vec3Argument.vec3())
@@ -52,7 +58,7 @@ public class WITSCommand {
             })
         )));
 
-        dispatcher.register(Commands.literal(commandString).redirect(source));
+        dispatcher.register(Commands.literal(opCommandString).redirect(source2));
     }
 
     private static void listStructuresAtSpot(ServerLevel level, Coordinates coordinates, boolean callerPosition, CommandContext<CommandSourceStack> cs) {
