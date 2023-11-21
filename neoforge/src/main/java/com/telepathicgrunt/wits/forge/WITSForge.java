35cf882d301cbe945fc3fcf8d8d0d050c66803b6
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-11-20 21:32:46 -0500

    fix up to run neoforge code

--- neoforge/src/main/java/com/telepathicgrunt/wits/forge/WITSForge.java
+++ neoforge/src/main/java/com/telepathicgrunt/wits/forge/WITSForge.java
@@ -2,17 +2,17 @@ package com.telepathicgrunt.wits.forge;
 
 import com.telepathicgrunt.wits.WITS;
 import com.telepathicgrunt.wits.commands.WITSCommand;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
 
 
 @Mod(WITS.MODID)
 public class WITSForge {
 
     public WITSForge() {
-        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+        IEventBus forgeBus = NeoForge.EVENT_BUS;
         forgeBus.addListener(this::registerCommand);
     }
 
