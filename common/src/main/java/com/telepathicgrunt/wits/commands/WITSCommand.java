8bcc88c21615a3fa13cfcd153a7e12cee2cb7cd5
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-07-11 11:40:00 -0400

    fix port

--- common/src/main/java/com/telepathicgrunt/wits/commands/WITSCommand.java
+++ common/src/main/java/com/telepathicgrunt/wits/commands/WITSCommand.java
@@ -13,14 +13,11 @@ import net.minecraft.commands.arguments.coordinates.WorldCoordinates;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentUtils;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.contents.PlainTextContents;
 import net.minecraft.resources.Identifier;
-import net.minecraft.network.chat.ComponentUtils;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.permissions.PermissionSet;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureStart;
@@ -87,32 +84,17 @@ public class WITSCommand {
             component = MutableComponent.create(PlainTextContents.create("Structure(s) at your location:"));
         }
         else {
-            component = MutableComponent.create(PlainTextContents.create("Structure(s) at " + centerPos.toShortString() + ":"));
+            component = MutableComponent.create(PlainTextContents.create("Structure(s) at "))
+                    .append(Component.translatable("chat.coordinates", centerPos.getX(), centerPos.getY(), centerPos.getZ()))
+                    .append(":");
         }
 
         for (Structure structure : structures) {
             Identifier key = level.registryAccess().lookupOrThrow(Registries.STRUCTURE).getKey(structure);
             component.append(Component.literal("\n -").withStyle(ChatFormatting.RESET))
-                    .append(Component.literal(key.toString()).withStyle(ChatFormatting.GOLD));
+                    .append(ComponentUtils.copyOnClickText(key.toString()).withStyle(ChatFormatting.GOLD));
         }
 
         cs.getSource().sendSuccess(() -> component, !cs.getSource().isPlayer());
-
-        
-        MutableComponent currentComponent = Component.empty();
-        if (callerPosition) {
-            currentComponent.append("Structure(s) at your location:");
-        }
-        else {
-            currentComponent.append("Structure(s) at ").append(Component.translatable("chat.coordinates", centerPos.getX(), centerPos.getY(), centerPos.getZ())).append(":");
-        }
-
-        for (Structure structure : structures) {
-            ResourceLocation key = level.registryAccess().registryOrThrow(Registries.STRUCTURE).getKey(structure);
-            currentComponent.append("§r\n - §6");
-            currentComponent.append(ComponentUtils.copyOnClickText(String.valueOf(key)));
-        }
-
-        cs.getSource().sendSuccess(() -> currentComponent, !cs.getSource().isPlayer());
     }
 }
